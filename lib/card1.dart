a60707e53bed1e76c7cbd83e501108ed0731967f
Mihary Joel ANDRIAMILANTO <hei.mihary.2@gmail.com>
2022-10-01 23:10:52 +0300

    feat: add card

---
+++ lib/card1.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+import 'fooderlich_theme.dart';
+class Card1 extends StatelessWidget{
+  const Card1({Key? key}) : super(key: key);
+// 1
+  final String category = 'Editor\'s Choice';
+  final String title = 'The Art of Dough';
+  final String description = 'Learn to make the perfect bread.';
+  final String chef = 'Ray Wenderlich';
+// 2
+  @override
+  Widget build(BuildContext context) {
+// 3
+    return Center(
+// TODO: Card1 Decorate Container
+      child: Container(
+      // TODO: Add a stack of text
+        // 1
+        padding: const EdgeInsets.all(16),
+        // 2
+        constraints: const BoxConstraints.expand(
+          width: 350,
+          height: 450,
+          ),
+        decoration: const BoxDecoration(
+           image: DecorationImage(
+              image: AssetImage('assets/mag1.png'),
+        fit: BoxFit.cover,
+          ),
+          borderRadius: BorderRadius.all(Radius.circular(10.0)
+          ),
+        ),
+        child: Stack(
+          children: [
+            Text(
+              category,
+              style: FooderlichTheme.darkTextTheme.bodyText1,
+            ),
+            Text(
+              title,
+              style: FooderlichTheme.darkTextTheme.headline5,
+            ),
+            Text(
+              description,
+              style: FooderlichTheme.darkTextTheme.bodyText1,
+            ),
+            Text(
+              chef,
+              style: FooderlichTheme.darkTextTheme.bodyText1,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
