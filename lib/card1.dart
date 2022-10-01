6b44dce9887f4d2c8d266797ef7d07462a9085ed
Mihary Joel ANDRIAMILANTO <hei.mihary.2@gmail.com>
2022-10-01 23:44:05 +0300

    update: card1

--- lib/card1.dart
+++ lib/card1.dart
@@ -33,22 +33,37 @@ class Card1 extends StatelessWidget{
         ),
         child: Stack(
           children: [
-            Text(
+              // 8
+              Text(
               category,
               style: FooderlichTheme.darkTextTheme.bodyText1,
-            ),
-            Text(
-              title,
-              style: FooderlichTheme.darkTextTheme.headline5,
-            ),
-            Text(
-              description,
-              style: FooderlichTheme.darkTextTheme.bodyText1,
-            ),
-            Text(
-              chef,
-              style: FooderlichTheme.darkTextTheme.bodyText1,
-            ),
+              ),
+          // 9
+              Positioned(
+              child: Text(
+                title,
+                style: FooderlichTheme.darkTextTheme.headline2,
+                ),
+                top: 20,
+                ),
+          // 10
+              Positioned(
+                  child: Text(
+                  description,
+                  style: FooderlichTheme.darkTextTheme.bodyText1,
+                  ),
+                bottom: 30,
+                right: 0,
+              ),
+          // 11
+            Positioned(
+              child: Text(
+                chef,
+                style: FooderlichTheme.darkTextTheme.bodyText1,
+              ),
+              bottom: 10,
+              right: 0,
+            )
           ],
         ),
       ),
